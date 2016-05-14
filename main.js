658172f08ef5c778a0af595320abee119c08f2dc
dibyatanoy <dibyatanoy.bhattacharjee@yale.edu>
2016-05-14 16:24:09 +0600

    Allowed handling of trailing and/or leading spaces after course codes

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 var codeMenu = false;
 
+function reverse(s){
+
+	return s.split("").reverse().join("");
+}
 
 searchYBBCode = function(word){
 
 	var query = word.selectionText;
+
 
 	query = query.replace(/\s/g, "+");
 
@@ -27,7 +32,23 @@ courseCodeEnabler = function(word){
 
 	var query = word;
 
-	if(query.length == 8 && query.search(/\s/) == 4 && codeMenu == false){
+	if(query.charAt(0) == " "){
+
+		query = query.replace(/\s+/, "");
+	}
+
+	query = reverse(query);
+
+	if(query.charAt(0) == " "){
+
+		query = query.replace(/\s+/, "");
+	}
+
+	query = reverse(query);
+
+	console.log(query);
+
+	if(query.length == 8 && query.search(/\s/) == 4){
 
 		childMenuCode = chrome.contextMenus.update("childMenuCode", {
 			title: "By course code",
@@ -38,7 +59,7 @@ courseCodeEnabler = function(word){
 
 		codeMenu = true;
 
-	}else if((query.length != 8 || query.search(/\s/) != 4) && codeMenu == true){
+	}else{
 
 		codeMenu = false;
 		chrome.contextMenus.update("childMenuCode", {
